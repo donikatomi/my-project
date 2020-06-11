fe223112a855cce0e792a865e3e5c6379c41e5ca
Donika Tomiqi <donikatomiqi@gmail.com>
2020-06-11 13:41:35 +0200

    post loop and custom body class

--- wp-content/themes/awesometheme/header.php
+++ wp-content/themes/awesometheme/header.php
@@ -6,6 +6,14 @@
     <title>Awesome Theme</title>
     <?php wp_head() ?>
 </head>
-<body>
+
+<?php 
+    if( is_front_page() ):
+        $awesome_classes = array('awesome-class', 'my-class');
+    else:
+        $awesome_classes = null;
+    endif
+?> 
+<body <?php body_class($awesome_classes) ?>>
     <?php wp_nav_menu(array('theme_location'=>'primary')) ?>
     
